73634998b83c29ce0baf53d6d095d81dfcf91bd8
alumno <alumno@lab6pc00>
2019-08-31 10:41:03 -0300

    solo suma

--- clasedos/src/clasedos.c
+++ clasedos/src/clasedos.c
@@ -10,8 +10,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+int suma (int *numeroUno,int *numeroDos);
+int main(void)
+{
+int numeroUno;
+int numeroDos;
+int resultado=0;
+ printf("ingrese primero numero");
+ scanf("%d",&numeroUno);
+ printf("ingrese segundo numero");
+ scanf("%d",&numeroDos);
+ resultado=suma(&numeroUno, &numeroDos);
+ printf("el resultado es %d",resultado);
+}
+int suma (int *numeroUno,int *numeroDos){
+	int auxiliar;
+	auxiliar=*numeroUno+*numeroDos;
+	return auxiliar;
 }
