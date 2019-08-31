c6e304e17fa6f7ae1d46574bef551ff675716262
alumno <alumno@lab6pc00>
2019-08-31 12:32:45 -0300

    Cambios

--- clasedos/src/clasedos.c
+++ clasedos/src/clasedos.c
@@ -10,19 +10,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-int suma (int *numeroUno,int *numeroDos);
+int suma (int numeroUno,int numeroDos);
+int resta (int numeroUno,int numeroDos);
+int divicion (int numeroUno,int numeroDos);
+int multiplicacion (int numeroUno,int numeroDos);
+int pedirNumero(char *mensaje);
 int main(void)
 {
-int numeroUno;
-int numeroDos;
-int resultado=0;
- printf("ingrese primero numero");
- scanf("%d",&numeroUno);
- printf("ingrese segundo numero");
- scanf("%d",&numeroDos);
- resultado=suma(&numeroUno, &numeroDos);
- printf("el resultado es %d",resultado);
+ int numero1;
+ int numero2;
+ int operacion;;
+ numero1=pedirNumero("ingrese primer numero");
+ numero2=pedirNumero("ingrese segundo numero");
+ operacion=pedirNumero("ingese operacion (1 suma , 2 restar 3 divide 4 multiplica)");
+ if(hacerCalculo(numero1,numero2,operacion)==0){
+	printf("error");
+ }
 }
-int suma (int *numeroUno,int *numeroDos){
-	return *numeroUno+*numeroDos;
+
+int pedirNumero(char *mensaje){
+	int numero;
+	printf(mensaje);
+	scanf("%d",&numero);
+	return numero;
+}
+
+int suma (int numeroUno,int numeroDos){
+	return numeroUno+numeroDos;
+}
+int resta (int numeroUno,int numeroDos){
+	return numeroUno-numeroDos;
+}
+int divicion (int numeroUno,int numeroDos){
+	return numeroUno/numeroDos;
+}
+int multiplicacion (int numeroUno,int numeroDos){
+	return numeroUno*numeroDos;
 }
